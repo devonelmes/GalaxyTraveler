c09219d99975888af2751607a3e350ebed7ce1d0
Melissa Cote <diamondmeadows22@gmail.com>
2025-11-11 17:39:09 -0500

    added Galaxy class, weighted mean calculation, main logic to read csv file and create/store objects

---
+++ Galaxy.py
@@ -0,0 +1,48 @@
+class Galaxy:
+    def __init__(self, name):
+        self.name = name # galaxy id
+        self.data = [] # list of dictionaries containing keys mm (distance modulus, m-M), err (error on distance modulus), and mpc (distance in Megaparsecs)
+        self.distance = None # distance in Mpc (Megaparsecs)
+
+    def add_data(self, mm, err, mpc):
+        self.data.append({"mm" : mm, "err" : err, "mpc" : mpc})
+
+    def set_distance(self):
+        # If there is only one galaxy in the database with the ID, set distance to the Mpc given in data
+        if len(self.data) == 1:
+            self.distance = self.data[0]["mpc"]
+
+        else:
+            # For multiple entries with the same ID, calculate a weighted mean of the distance modulus of each entry, then convert to Mpc
+            numerator = []
+            denominator = []
+
+            for entry in self.data:
+                # Calculate inverse square of error for weight and add to denominator list
+                weight = 1 / (entry["err"] ** 2)
+                denominator.append(weight)
+
+                # Multiply distance modulus (m-M) and weight, then add to numerator list
+                numerator.append(weight * entry["mm"])
+
+            # Calculate weighted mean of distance modulus
+            d_mod = sum(numerator) / sum(denominator)
+
+            # Measurement of Distances: https://ned.ipac.caltech.edu/level5/Sept11/Freedman/Freedman3.html
+            # https://en.wikipedia.org/wiki/Distance_modulus
+            # m - M (distance modulus) = 5 log d - 5. d is parsecs, log base 10
+            # https://astro.wku.edu/labs/m100/mags.html
+            # Solve for d to get distance in parsecs: d = 10 ** (0.2 * (dMod + 5))
+            # Convert parsecs to Mpc: d / 10 ** 6
+            d_parsecs = 10 ** (0.2 * (d_mod + 5))
+            d_mpc = d_parsecs / 10 ** 6
+
+            self.distance = d_mpc
+
+    def get_distance(self):
+        return self.distance
+
+    def print_galaxy(self):
+        print("Galaxy Name: " + self.name)
+        print("Galaxy Distance (Mpc): " + str(self.distance))
+        print()
--- GalaxyTraveler.py
+++ GalaxyTraveler.py
@@ -1,3 +1,70 @@
 # COP3530 Final Project
 # Devon Elmes, Lang Cao, and Melissa Cote
 # Let's get this bread
+import csv
+from Galaxy import Galaxy
+
+if __name__ == '__main__':
+    # https://www.geeksforgeeks.org/pandas/reading-csv-files-in-python/
+    # https://www.geeksforgeeks.org/python/working-csv-files-python/
+    with open('NED30.5.1-D-17.1.2-20200415.csv', mode = 'r') as file:
+        # Skip the first 12 lines containing metadata
+        for row in range(13):
+            next(file)
+
+        csvFile = csv.reader(file)
+        seen = set()
+        current_id = None
+        grouped = True
+        line_num = 0
+
+        # Stores galaxy objects
+        galaxies = []
+        # Dictionary storing id as key and index in galaxies list as value. Helper for filling galaxies list.
+        id_index = {}
+
+        # Read csv file and create galaxy objects
+        invalid_count = 0
+        for i, row in enumerate(csvFile):
+            try:
+                galaxy_id = row[3] # NED "Preferred Object Name" for the host galaxy
+                mm = float(row[4]) # Distance Modulus expressed in mag
+                err = float(row[5]) # Quoted (one-sigma) statistical (random) error on the distance modulus
+                mpc = float(row[6]) # Metric distance (in units of Mpc)
+                if err == 0:
+                    raise ValueError("error is 0") # If err == 0, will cause denominator in weighted mean to be 0
+                if len(galaxy_id) == 0:
+                    raise ValueError("galaxy id is empty")
+            except ValueError:
+                invalid_count += 1
+                continue
+
+            # If galaxy has already been added to galaxies, add information from this row to its data list
+            if galaxy_id in id_index:
+                galaxies[id_index[galaxy_id]].add_data(mm, err, mpc)
+            # Create galaxy if not in list and add its index in galaxies to id_index
+            else:
+                new_galaxy = Galaxy(galaxy_id)
+                new_galaxy.add_data(mm, err, mpc)
+                galaxies.append(new_galaxy)
+                id_index[galaxy_id] = len(galaxies) - 1
+
+        # Set distances for all galaxies
+        for galaxy in galaxies:
+            galaxy.set_distance()
+
+        print("Invalid rows in file: " + str(invalid_count))
+        print("Galaxies in list: " + str(len(galaxies)))
+
+
+
+
+
+
+
+
+
+
+
+
+
