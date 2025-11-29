7e65f03b5c01ebc042ac6ffaa599938b2fe8f7ff
Devon Elmes <devonelmes@ufl.edu>
2025-11-29 07:25:11 -0800

    Removed unused lines of code, added comments to __init__()

--- ui.py
+++ ui.py
@@ -1,8 +1,6 @@
+"""References:
+        https://customtkinter.tomschimansky.com/
 """
-Resources:
-    https://customtkinter.tomschimansky.com/
-"""
-import tkinter as tk
 import customtkinter as ctk
 from PIL import Image
 from galaxy import Galaxy
@@ -15,17 +13,17 @@ class App(ctk.CTk):
 
     def __init__(self):
         super().__init__()
-
+        # Set app window title and dimensions.
         self.title("✴ Travel Destinations: Nearest Galaxies ✴")
         self.geometry("1300x800")
-
+        # Set background image.
         image_path = join("Starfield Images", "Starfield 8 - 1024x1024.png")
         pil_image = Image.open(image_path)
         self.bg_img = ctk.CTkImage(light_image=pil_image, dark_image=pil_image, size=(self.DIMENSIONS[0], self.DIMENSIONS[1]))
-
         self.bg_label = ctk.CTkLabel(self, image=self.bg_img, text="")
         self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
-
+        # Partition window (Menu - top right, 500x300; Scroll galaxy display - bottom left, 500x500; graph - right, 800x800)
+        # code
         # Sidebar buttons
         ctk.CTkLabel(self, text="Options", font=("Helvetica", 18)).pack(pady=20)
 
